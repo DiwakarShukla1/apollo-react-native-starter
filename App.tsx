142b2f99c928e666abc66200ccb7a8496fe5f937
Diwakar Shukla <diwakarshukla14@gmail.com>
2018-04-27 16:34:37 +0530

    apollo setuped

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { AppRegistry } from "react-native";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import First from "./Query";
+
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql",
+});
 
 class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Hello1 Great Going</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <First />
+      </ApolloProvider>
     );
   }
 }
