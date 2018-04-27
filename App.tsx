ac0ee41e0b96c9842eac3901853d03c79a74318f
Diwakar Shukla <diwakarshukla14@gmail.com>
2018-04-27 12:48:26 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello1 Great Going</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
